c0a88918b8d6a5863337b5dc8ae6a7136dc9fdf2
Deepanshu Pant <deepanshupant8@gmail.com>
2025-01-02 16:39:42 +0530

    Heroku Procfile

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BABEL_DEFAULT_LOCALE = 'en_IN'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
